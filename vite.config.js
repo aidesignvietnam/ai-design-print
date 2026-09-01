523b4208bef86ddd6a3b343508ee7ef33f0ea9c5
aidesignvietnam <quangcaominhpha@gmail.com>
2026-09-01 16:43:32 +0700

    Update base URL configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ai-design-print/"
+  base: process.env.VERCEL
+    ? "/"
+    : "/ai-design-print/"
 });
